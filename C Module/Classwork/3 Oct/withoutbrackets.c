41ccd701bf4f72dc75aca4f33308bb6820b8b0fd
Adbel <141557073+Adbelkhan@users.noreply.github.com>
2025-10-02 11:49:37 +0530

    Add files via upload

---
+++ C Module/Classwork/3 Oct/withoutbrackets.c
@@ -0,0 +1,33 @@
+void main ()
+{
+	int a = 10, b = 30, c = 20;
+	if(a>b)
+	{
+		if(a>c)
+		  printf("a is greater");
+	    else
+	      printf("c is greater");
+	}
+	else 
+	{
+		if(b>c)
+		  printf("b is greater");
+		else
+		  printf("c is greater");
+	}
+}
+
+
+
+
+//void main()
+//{
+//	int marks = 86;
+//	if(marks>=75)
+//	   printf("A+");
+//    else if(marks>=65)
+//       printf("B+");
+//    else if(marks>=45)
+//       printf("C+");
+//    else if()
+//}
