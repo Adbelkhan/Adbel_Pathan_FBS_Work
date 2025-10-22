eb7466fd65718d2acb8a349e893e1fe34b0f76ce
Adbel <141557073+Adbelkhan@users.noreply.github.com>
2025-10-22 19:49:13 +0530

    Add files via upload

---
+++ C Module/Assignments/Assignment_6_/a_1_1.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+void evenOdd();
+
+	void main()
+	{	
+	   evenOdd();	
+	}
+	
+	void evenOdd()
+	{
+	   	int no ;
+	   
+	    printf("Enter the no \n");
+		scanf("%d", &no);
+	   
+	    if(no%2==0)
+	      printf("No is even");
+       	else
+		   printf("No is Odd");
+	
+	
+   }
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
