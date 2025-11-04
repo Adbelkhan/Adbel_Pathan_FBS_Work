ff2337f26d200fef1e24b5a8fdea2b1fe222bcbe
Adbel <141557073+Adbelkhan@users.noreply.github.com>
2025-11-04 17:27:07 +0530

    Add files via upload

---
+++ C Module/Classwork/11 Oct/pattern.c
@@ -0,0 +1,25 @@
+void main()
+{
+//	for(int i=1; i<=4; i++)
+//	{
+//		for(int j=1; j<=4; j++)
+//		{
+//			//if(i==1&&j>=3 || i==7 || j==7&& i>=4 )
+//			printf(" ");
+//		}
+//	//	printf("\n");
+//	}
+//	printf("*");
+//	printf("\n");
+	
+//	for(int i=1; i<=7; i++ )
+      
+//	{
+        int i;
+		if(i<=3 && i>=4)
+		printf(" ");
+		else
+	 	printf("* ");
+//	}
+	printf("\n");
+}
