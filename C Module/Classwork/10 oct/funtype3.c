5640f5966bf27371cc2fbd64fdee0a24921ac698
Adbel <141557073+Adbelkhan@users.noreply.github.com>
2025-11-04 17:27:58 +0530

    Add files via upload

---
+++ C Module/Classwork/10 oct/funtype3.c
@@ -0,0 +1,25 @@
+void main()
+{
+	int a=10, b=20,c;
+	char ch='x';
+	double d=10.2;
+	
+	fun1(a,b,c,ch,d,10,20.2);
+	fun2(a+2,b*3+c,'a','y',10+2);
+	fun3('x','d',b+d,a*b*d);
+}
+
+void fun1(int a, int b, int c, char d, double f, int e, double o)
+{
+	
+}
+
+void fun2(int q, int w, char a, char y, int c)
+{
+	
+}
+
+void fun3(char x, char d, double e, double r)
+{
+	
+}
